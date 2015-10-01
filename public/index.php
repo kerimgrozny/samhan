ebb3ccb20a9de46fda280758eace345388cc49cd
Kerim Timirbulatov <kerimgrozny@gmail.com>
2015-10-01 23:40:21 +0300

    add whatver

--- public/index.php
+++ public/index.php
@@ -19,37 +19,31 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<nav class="navbar navbar-inverse">
-	  	<div class="container-fluid">
+	<nav class="navbar navbar-inverse navbar-static-top" id="navbar">
+	  	<div class="container-fluid" id="headerListItems">
 	    	<div class="navbar-header">
-	      		<a class="navbar-brand" href="#">WebSiteName</a>
+	      		<a class="navbar-brand" id="brand" href="#">ООО Интерактив</a>
 	    	</div>
-	    	<div>
+	    	<div id="mainList">
 	      		<ul class="nav navbar-nav">
-	        		<li class="active"><a href="#">Home</a></li>
-	        		<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-	          			<ul class="dropdown-menu">
-	            			<li><a href="#">Page 1-1</a></li>
-	            			<li><a href="#">Page 1-2</a></li>
-	            			<li><a href="#">Page 1-3</a></li>
-	          			</ul>
-	        		</li>
-	        		<li><a href="#">Page 2</a></li>
-	        		<li><a href="#">Page 3</a></li>
+	        		<li><a href="#">Главная</a></li>
+	        		<li><a href="#">Проекты</a></li>
+	        		<li><a href="#">Услуги</a></li>
+	        		<li><a href="#">О Нас</a></li>
+	        		<li><a href="#">Контакты</a></li>
 	      		</ul>
+	      	</div>
+	      	<div id="userForm">
 	     		<ul class="nav navbar-nav navbar-right">
-	        		<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	        		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	        		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Войти</a></li>
+	        		<li><a href="#"><span class="glyphicon glyphicon-user"></span> Регистрация</a></li>
 	      		</ul>
 	    	</div>
 	    </div>
 	</nav> 
-	<div class="container">
-	  	<div class="jumbotron">
-	    	<h1>My first Bootstrap website!</h1>      
-	    	<p>This page will grow as we add more and more components from Bootstrap...</p>      
-	    	<a href="#" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span> Search</a>
-	    </div>
+	<div class="container" id="content">
+	    	<h1 class="text-center">ООО Интерактив</h1>      
+	    	<p>This page will grow as we add more and more components from Bootstrap...</p>
 	  	<div class="row">
 	    	<div class="col-md-3">
 	        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -60,23 +54,9 @@
 		    <div class="col-md-3"> 
 		      	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
 		    </div>
-		    <div class="col-md-3">
-		      	<ul class="nav nav-pills nav-stacked">
-		        	<li class="active"><a href="#">Home</a></li>
-		        	<li class="dropdown">
-		          	<a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu 1 <span class="caret"></span></a>
-			          	<ul class="dropdown-menu">
-				            <li><a href="#">Submenu 1-1</a></li>
-				            <li><a href="#">Submenu 1-2</a></li>
-				            <li><a href="#">Submenu 1-3</a></li>                        
-			          	</ul>
-		       		</li>
-			        <li><a href="#">Menu 2</a></li>
-			        <li><a href="#">Menu 3</a></li>
-		      	</ul>
-		    </div>
 		    <div class="clearfix visible-lg"></div>
 	  	</div>
+
 	</div>
 	<nav class="navbar navbar-inverse navbar-fixed-bottom">
 	  	<div class="container-fluid">
